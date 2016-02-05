5f55ac46b232fc362ab740516a9c5cddb7634aa7
Matthew Harris <mharris@Matthews-MacBook-Pro.local>
2016-02-04 21:18:59 -0700

    getting rid of java

--- app/src/main/java/com/eggman/circleciandroid/model/Project.java
+++
@@ -1,8 +0,0 @@
-package com.eggman.circleciandroid.model;
-
-/**
- * Created by mharris on 2/4/16.
- * DispatchHealth.
- */
-public class Project {
-}
---
+++ app/src/main/java/com/eggman/circleciandroid/model/Project.kt
@@ -0,0 +1,8 @@
+package com.eggman.circleciandroid.model
+
+/**
+ * Created by mharris on 2/4/16.
+ * DispatchHealth.
+ */
+data class Project (val reponame:String, val username:String, val language:String, val vcsUrl:String) {
+}
